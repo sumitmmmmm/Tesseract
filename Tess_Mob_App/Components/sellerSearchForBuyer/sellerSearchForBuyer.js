a63441030e0aa30441abb7b99f84cb6b15c41a00
MD <imtiaztfs@outlook.com>
2021-03-14 21:09:41 +0530

    1st commit

--- Tess_Mob_App/Components/sellerSearchForBuyer/sellerSearchForBuyer.js
+++ Tess_Mob_App/Components/sellerSearchForBuyer/sellerSearchForBuyer.js
@@ -6,6 +6,8 @@ import Modal from 'react-native-modal'
 import Footer from '../Footer/footer';
 import Header from '../Header/header';
 import { ScrollView } from 'react-native-gesture-handler';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faShoppingCart, faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 export default class SellerSeachForBuyer extends React.Component{
     constructor(props){
@@ -15,10 +17,46 @@ export default class SellerSeachForBuyer extends React.Component{
         }
     }
 
+    height = Dimensions.get("window").height;
+    width = Dimensions.get("window").width;
+
     render(){
         return(
-            <View style={{justifyContent: 'center'}}>
-                <Header />
+            <View style={{}}>
+                <View style={{justifyContent: 'center'}}>
+                    <Header />
+                </View>
+
+                <View style={{justifyContent: 'center', borderRadius: 5, borderWidth: 3}}>
+                    <View style={{justifyContent: 'center', alignItems: 'center', paddingVertical: this.height*0.02}}>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold'}}>Select Shopping Style</Text>
+                    </View>
+
+                    <View style={{flexDirection: 'row', alignItems: 'center', paddingVertical: this.height*0.02}}>
+                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center'}} onPress={this.registerBuyer}>
+                                <View style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#3333CC', height: 60, width: 60, borderRadius: 30}}>
+                                    <FontAwesomeIcon icon={faUser} size={30} style={{color:"#ffff"}}/>
+                                </View>
+                                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={{color: '#3333CC', fontWeight: 'bold'}}>Single Shopping</Text>
+                                </View>                          
+                            </TouchableOpacity>
+                        </View> 
+
+                        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                            <TouchableOpacity style={{justifyContent: 'center', alignItems: 'center'}} onPress={this.registerBuyer}>
+                                <View style={{justifyContent: 'center', alignItems: 'center', backgroundColor: '#3333CC', height: 60, width: 60, borderRadius: 30}}>
+                                    <FontAwesomeIcon icon={faUsers} size={30} style={{color:"#ffff"}}/>
+                                </View>
+                                <View style={{justifyContent: 'center', alignItems: 'center'}}>
+                                    <Text style={{color: '#3333CC', fontWeight: 'bold'}}>Group Shopping</Text>
+                                </View>                          
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+                
             </View>
         )
     }
